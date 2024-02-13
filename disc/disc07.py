61c098b82d1db0596feaa95441ca559565ad0a73
Aierlanta <aliceaierlanta@gmail.com>
2024-02-13 21:28:11 +0800

    modified:   disc/disc07.py

--- disc/disc07.py
+++ disc/disc07.py
@@ -214,3 +214,38 @@ class TeamMember:
         most recent relay the current TeamMember has participated in.
         """
         return self.history
+
+
+class Cat:
+    def __init__(self, name:str, owner:str, lives:int=9):
+        self.is_alive = True
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+
+    def talk(self):
+        return self.name + " says meow!"
+
+    @classmethod
+    def cat_creator(cls, owner:str):
+        """
+        Returns a new instance of a Cat.
+
+        This instance's name is "[owner]'s Cat", with
+        [owner] being the name of its owner.
+
+        >>> cat1 = Cat.cat_creator("Bryce")
+        >>> isinstance(cat1, Cat)
+        True
+        >>> cat1.owner
+        'Bryce'
+        >>> cat1.name
+        "Bryce's Cat"
+        >>> cat2 = Cat.cat_creator("Tyler")
+        >>> cat2.owner
+        'Tyler'
+        >>> cat2.name
+        "Tyler's Cat"
+        """
+        name = f"{owner}'s Cat"
+        return cls(name, owner)
