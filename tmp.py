cfc365aecec10bdb7faedb0106e8aa09a7bb4e89
Aierlanta <aliceaierlanta@gmail.com>
2024-02-14 23:43:05 +0800

    new file:   hw06/.ok_history 	new file:   hw06/.ok_storage 	new file:   hw06/adventure.py 	new file:   hw06/classes.py 	new file:   hw06/construct_check.py 	new file:   hw06/data.py 	new file:   hw06/hw06.ok 	new file:   hw06/hw06.py 	new file:   hw06/me.py 	new file:   hw06/ok 	new file:   hw06/tests/__init__.py 	modified:   tmp.py

--- tmp.py
+++ tmp.py
@@ -1,65 +1,39 @@
-class TeamMember:
-    """>>> adder = TeamMember(lambda x: x + 1) # team member at front
-    >>> adder2 = TeamMember(lambda x: x + 2, adder) # team member 2
-    >>> multiplier = TeamMember(lambda x: x * 5, adder2) # team member 3
-    >>> adder.relay_history() # relay history starts off as empty
-    []
-    >>> adder.relay_calculate(5) # 5 + 1
-    6
-    >>> adder2.relay_calculate(5) # (5 + 1) + 2
-    8
-    >>> multiplier.relay_calculate(5) # (((5 + 1) + 2) * 5)
-    40
-    >>> multiplier.relay_history() # history of answers from the most recent relay multiplier participated in
-    [6, 8, 40]
-    >>> adder.relay_history()
-    [6]
-    >>> multiplier.relay_calculate(4) # (((4 + 1) + 2) * 5)
-    35
-    >>> multiplier.relay_history()
-    [5, 7, 35]
-    >>> adder.relay_history() # adder participated most recently in multiplier.relay_calculate(4), where it gave the answer 5
-    [5]
-    >>> adder.relay_calculate(1)
-    2
-    >>> adder.relay_history() # adder participated most recently in adder.relay_calculate(1), where it gave the answer 2
-    [2]
-    >>> multiplier.relay_history() # but the most relay multiplier participated in is still multiplier.relay_calculate(4)
-    [5, 7, 35]
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Nothing left to vend. Please restock.'
+    >>> v.add_funds(15)
+    'Nothing left to vend. Please restock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'Please add $10 more funds.'
+    >>> v.add_funds(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'Please add $3 more funds.'
+    >>> v.add_funds(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.add_funds(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.add_funds(15)
+    'Nothing left to vend. Please restock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.add_funds(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
     """
-    def __init__(self, operation, prev_member=None):
-        """
-        A TeamMember object is instantiated by taking in an `operation`
-        and a TeamMember object `prev_member`, which is the team member
-        who "sits in front of" this current team member. A TeamMember also
-        tracks a `history` list, which contains the answers given by
-        each individual team member.
-        """
-        self.history = []
-        self.operation = operation # Store the operation as an attribute
-        self.prev_member = prev_member # Store the previous member as an attribute
 
-    def relay_calculate(self, x):
-        """
-        The relay_calculate method takes in a number `x` and performs a
-        relay by passing in `x` to the first team member's `operation`.
-        Then, that answer is passed to the next member's operation, etc. until
-        we get to the current TeamMember, in which case we return the
-        final answer, `result`. 
-        """
-        if self.prev_member is None: # Check if this is the first member in the relay
-            result = self.operation(x) # Apply the operation to x
-        else:
-            result = self.operation(self.prev_member.relay_calculate(x)) # Apply the operation to the result of the previous member
-        self.history.append(result) # Append the result to the history list
-        return result # Return the final result
-
-    def relay_history(self):
-        """
-        Returns a list of the answers given by each team member in the
-        most recent relay the current TeamMember has participated in.
-        """
-        if self.prev_member is None: # Check if this is the first member in the relay
-            return self.history # Return the history list
-        else:
-            return self.prev_member.relay_history() + self.history # Return the history list of the previous member plus the current member
+    "*** YOUR CODE HERE ***"
