abfad9d2f9f12baab2a65302fd9c06fad86242bc
Aierlanta <aliceaierlanta@gmail.com>
2024-02-19 02:03:30 +0800

    modified:   tmp.py

--- tmp.py
+++ tmp.py
@@ -1,25 +1,60 @@
-class A:
-    def __init__(self, x):
-        self.x = x
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
 
-    def __repr__(self):
-        return self.x
 
-    def __str__(self):
-        return self.x * 2
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
 
 
-class B:
-    def __init__(self):
-        print("boo!")
-        self.a = []
-
-    def add_a(self, a):
-        self.a.append(a)
-
-    def __repr__(self):
-        print(len(self.a))
-        ret = ""
-        for a in self.a:
-            ret += str(a)
-        return ret
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    """
+    def constrained_count_small(total, largest_coin):
+        if total == 0:
+            return 1
+        if total < 0:
+            return 0
+        if largest_coin == None:
+            return 0
+        without_coin = constrained_count_small(total, next_smaller_coin(largest_coin))
+        with_coin = constrained_count_small(total - largest_coin, largest_coin)
+        return without_coin + with_coin
+    return constrained_count_small(total, 25)
